8ae5e81afed59211bb520586457d6210c15271d9
Zhengyu Wang <zhengyu.wang@cygni.se>
2020-04-20 08:00:20 +0200

    Add encoding=UTF8

--- excel_2_csv_converter.py
+++ excel_2_csv_converter.py
@@ -7,24 +7,14 @@ import csv, os, xlrd
 def excel_to_csv():
     os.chdir('./excelSpreadsheets')
     for filename in os.listdir('.'):
-        print(f'{filename}')
-        wb = xlrd.open_workbook(filename)
+        wb = xlrd.open_workbook('spreadsheet-A.xlsx')
         # Loop through every sheet in the workbook
-        for sh in wb.sheets():
-            # Create the csv filename from the excel filename and sheet title
-            csv_file = open(f'output.csv', 'wb')
-
-            # Create the csv.writer object for the this csv file
-            csv_writer = csv.writer(csv_file)
-
-            # Write the rowData list to the csv file
-            for row in range(sh.nrows):
-                row_data = []
-                for col in range(sh.ncols):
-                    value = sh.cell(row, col).value
-                    row_data.append(value)
-                csv_writer.writerow(row_data)
-            csv_file.close()
+        sheet = wb.sheet_by_name('Sheet')
+        csv_file = open(f'{filename}.csv', 'w', encoding='utf8')
+        wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        for row_num in range(sheet.nrows):
+            wr.writerow(sheet.row_values(row_num))
+        csv_file.close()
 
 
 if __name__ == '__main__':
