528922e6922431094af3f77fab47280a7032990d
Zhengyu Wang <zhengyu.wang@cygni.se>
2020-04-20 11:05:33 +0200

    Correct filename to csv files

--- excel_2_csv_converter.py
+++ excel_2_csv_converter.py
@@ -2,15 +2,20 @@
 # A program that reads in all the Excel files in the current working directory
 # and outputs them as CSV files.
 import csv, os, xlrd
+import time
+from pathlib import Path
 
 
 def excel_to_csv():
+
     os.chdir('./excelSpreadsheets')
-    for filename in os.listdir('.'):
-        wb = xlrd.open_workbook('spreadsheet-A.xlsx')
-        # Loop through every sheet in the workbook
+    abs_path = os.path.abspath('.')
+    print(f'Searching xlsx files in {abs_path}...')
+    dir = Path(abs_path)
+    for filename in dir.glob('*.xlsx'):
+        wb = xlrd.open_workbook(filename)
         sheet = wb.sheet_by_name('Sheet')
-        csv_file = open(f'{filename}.csv', 'w', encoding='utf8')
+        csv_file = open(f'{filename.stem}.csv', 'w', encoding='utf8')
         wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for row_num in range(sheet.nrows):
             wr.writerow(sheet.row_values(row_num))
